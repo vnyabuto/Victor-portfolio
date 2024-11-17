00307598e0200e56500f110b1efee3db444f9aa1
victor Nyabuto <vnyabuto00@gmail.com>
2024-11-17 23:14:50 +0300

    Initial commit for Victor portfolio

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -4,29 +4,37 @@ import { whyChooseMe } from '../../sources';
 const About = () => {
   return (
     <section id="about">
-      <div className="wrapper">
-        <div className="section-header">
-          <h1 className="heading-1" data-aos="fade-right">
-            <span className="gradient-text">About Me</span>
-          </h1>
-          <h4 className="sub-title muted" data-aos="fade-left">
-          I am a dedicated Fullstack Software Developer with a strong proficiency in the MERN stack—MongoDB, Express.js, React, and Node.js. My passion lies in creating dynamic and responsive web applications that offer seamless user experiences and robust server-side solutions. Beyond the MERN stack, I am skilled in Java, Spring Boot, MySQL, C++, C#, and PHP, and I have experience with cloud platforms like AWS and Azure. I thrive in collaborative environments, leveraging Agile methodologies to deliver high-quality software. My commitment to continuous learning and innovation drives me to stay abreast of emerging technologies and to create impactful, scalable solutions. Let’s connect and build something transformative together.
-          </h4>
-        </div>
-        <div className="group">
-          {
-            whyChooseMe.map((list, index)=>(
-              <div className='flex-center group-item' data-aos="fade-right" key={index}>
-                <div className='flex-center icon-wrapper'>
-                  {list.icon}
-                </div>
-                <h4 className="title">{list.title}</h4>
-              </div>
-            ))
-          }
-        </div>
+    <div className="wrapper">
+      <div className="section-header">
+        <h1 className="heading-1" data-aos="fade-right">
+          <span className="gradient-text">About Me</span>
+        </h1>
+        <h4 className="sub-title muted" data-aos="fade-left">
+  I am a dedicated Data Scientist and Machine Learning Specialist with a strong background in Fullstack Software Development. My expertise encompasses advanced data science techniques, machine learning algorithms, and predictive modeling, complemented by my proficiency in the MERN stack—MongoDB, Express.js, React, and Node.js.
+  <br /><br />
+  My passion lies in creating dynamic and responsive web applications and data-driven solutions. I am adept at using technologies such as Python, scikit-learn, TensorFlow, and OpenCV for developing and deploying machine learning models. Additionally, I am skilled in Java, Spring Boot, MySQL, C++, C#, and PHP, with experience in cloud platforms like AWS and Azure.
+  <br />
+  I thrive in collaborative environments and utilize Agile methodologies to deliver high-quality software. My commitment to continuous learning and innovation drives me to stay abreast of emerging technologies and create impactful, scalable solutions. 
+  <br /><br />
+  Beyond my technical expertise, I am a seasoned karate coach with a black belt. I have successfully led teams like Karatina University to victory in tournaments from regionals to nationals, demonstrating leadership and discipline. Let’s connect and build something transformative together.
+</h4>
+
       </div>
-    </section>
+      <div className="group">
+        {
+          whyChooseMe.map((list, index) => (
+            <div className='flex-center group-item' data-aos="fade-right" key={index}>
+              <div className='flex-center icon-wrapper'>
+                {list.icon}
+              </div>
+              <h4 className="title">{list.title}</h4>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  </section>
+  
   )
 }
 
--- src/Components/Hero/index.jsx
+++ src/Components/Hero/index.jsx
@@ -6,35 +6,38 @@ import "./Hero.css";
 
 const Hero = () => {
   return (
-    <section id='hero'>
-      <div className="wrapper info-container">
-        <div className="column">
-          <h3 className='sub-title'>
-            Hi, I'm <span className="primary">Simon Meka</span>
-          </h3>
-          <h1 className="heading-1" data-aos="fade-right">
-            A <span className="gradient-text">Fullstack</span> Software Developer
-          </h1>
-          <p className="muted" data-aos="fade-up" data-aos-delay="200">
-            I am a passionate programmer with a robust proficiency in the comprehensive MERN stack, encompassing MongoDB, Express.js, React, and Node.js. My expertise extends to crafting dynamic and responsive web applications, leveraging cutting-edge technologies to build seamless user experiences and scalable server-side solutions.
-            <br /><br />
-            On the front-end, I excel in utilizing React to develop highly interactive user interfaces with state-of-the-art features, ensuring optimal performance and responsiveness. My skills in React Native also enable me to deliver cross-platform mobile applications with a native look and feel. On the back-end, I leverage Node.js and Express.js to build efficient, RESTful APIs and robust server-side applications, facilitating smooth communication between client and server.
-          </p>
-          <div className="flex-center buttons-wrapper">
-            <Link to ='services' smooth={true } className='btn primary' data-aos="fade-left" data-aos-delay="1200" data-aos-offset="50">Learn more</Link>
-            <Link to ='contact' smooth={true } className='btn' data-aos="fade-left" data-aos-delay="1500" data-aos-offset="50" >Get started</Link>
-          </div>
-        </div>
-        <div className="column hero-image" data-aos="fade-left" data-aos-delay="200">
-          <img src={me} alt="" />
-        </div>
+<section id='hero'>
+  <div className="wrapper info-container">
+    <div className="column">
+      <h3 className='sub-title'>
+        Hi, I'm <span className="primary">Victor Nyabuto</span>
+      </h3>
+      <h1 className="heading-1" data-aos="fade-right">
+        A <span className="gradient-text">Data Scientist</span> & <span className="gradient-text">Machine Learning Engineer</span>
+      </h1>
+      <p className="muted" data-aos="fade-up" data-aos-delay="200">
+        I am a dedicated expert in Data Science and Machine Learning, specializing in developing predictive models and implementing advanced algorithms to derive actionable insights from complex datasets.
+        <br /><br />
+        My expertise includes utilizing cutting-edge technologies such as Python, Pandas, Numpy, Matplotlib, Searborn, scikit-learn, TensorFlow, and OpenCV for building and deploying machine learning models. I excel in data preprocessing, feature engineering, model training and evaluation. My skills extend to creating data-driven solutions and visualizing results to support strategic decision-making.
+        <br /><br />
+        Additionally, I leverage Django for web development and integrate machine learning models into web applications to deliver interactive and intelligent user experiences.
+      </p>
+      <div className="flex-center buttons-wrapper">
+        <Link to='services' smooth={true} className='btn primary' data-aos="fade-left" data-aos-delay="1200" data-aos-offset="50">Learn more</Link>
+        <Link to='contact' smooth={true} className='btn' data-aos="fade-left" data-aos-delay="1500" data-aos-offset="50">Get started</Link>
       </div>
-      <div className="achievement-cluster">
-        <div className="wrapper">
-          <Achievement />
-        </div>
-      </div>
-    </section>
+    </div>
+    <div className="column hero-image" data-aos="fade-left" data-aos-delay="200">
+      <img src={me} alt="Victor Nyabuto" />
+    </div>
+  </div>
+  <div className="achievement-cluster">
+    <div className="wrapper">
+      <Achievement />
+    </div>
+  </div>
+</section>
+
   )
 }
 
