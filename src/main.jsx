187170a8e6c8e74babf77243aa4859d8948b3dd2
victor Nyabuto <vnyabuto00@gmail.com>
2026-01-09 20:06:28 +0300

    Fix main router setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,13 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { HashRouter as Router } from 'react-router-dom';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router> {/* Wrap your App component with the Router */}
-      <App />
-    </Router>
-  </React.StrictMode>
-);
-=======
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { HashRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 )
->>>>>>> 5b3a05d19eec8cf830028522b85aed9467d838e6
